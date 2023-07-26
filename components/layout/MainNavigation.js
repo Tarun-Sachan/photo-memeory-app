97d81a2c88456c9c00285b9149324926488ff676
tarunSachan <117214735+Tarun-Sachan@users.noreply.github.com>
2023-07-26 14:53:41 +0530

    Update MainNavigation.js

--- components/layout/MainNavigation.js
+++ components/layout/MainNavigation.js
@@ -6,7 +6,7 @@ function MainNavigation() {
 
   return (
     <header className={classes.header}>
-      <div className={classes.logo}>React Meetups</div>
+      <div className={classes.logo}>Photo Memory React </div>
       <nav>
         <ul>
           <li>
